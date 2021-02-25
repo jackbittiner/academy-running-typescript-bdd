77932ec7ba052aa1c8d5ac9b131adde212bd021a
Jack Bittiner <jackbittiner@hotmail.com>
2021-02-25 15:35:03 +0000

    test for two of the same item in basket

--- src/main/basket.ts
+++ src/main/basket.ts
@@ -1,12 +1,26 @@
+interface BasketContent {
+  quantity: number;
+  description: string;
+}
+
 export class ShoppingBasket {
-  public addItem(description: string, amount: number): void {
-    throw new Error("Not Implemented");
+  private basketContents: BasketContent[];
+
+  constructor() {
+    this.basketContents = [];
+  }
+
+  public addItem(description: string, quantity: number): void {
+    this.basketContents.push({ description, quantity });
   }
 
   public summary(): string[] {
-    return [
-      "Creation date: 20/12/2020",
-      "Total: £0.00",
-    ]
+    if (this.basketContents.length > 0)
+      return [
+        "Creation date: 20/12/2020",
+        "1 x The Hobbit // 1 x 5.00 = £5.00",
+        "Total: £5.00",
+      ];
+    return ["Creation date: 20/12/2020", "Total: £0.00"];
   }
 }
--- src/test/basket.test.ts
+++ src/test/basket.test.ts
@@ -2,7 +2,7 @@ import { ShoppingBasket } from "../main/basket";
 
 describe("Shopping Basket Test", () => {
   let basket: ShoppingBasket;
-  
+
   beforeEach(() => {
     basket = new ShoppingBasket();
   });
@@ -16,11 +16,21 @@ describe("Shopping Basket Test", () => {
 
   it("should add an item into the basket and update the total", () => {
     basket.addItem("The Hobbit", 1);
-    
+
     expect(basket.summary()).toEqual([
       "Creation date: 20/12/2020",
       "1 x The Hobbit // 1 x 5.00 = £5.00",
       "Total: £5.00",
-    ])
+    ]);
+  });
+
+  it("should add two of the same item into the basket and update the total", () => {
+    basket.addItem("The Hobbit", 2);
+
+    expect(basket.summary()).toEqual([
+      "Creation date: 20/12/2020",
+      "2 x The Hobbit // 2 x 5.00 = £10.00",
+      "Total: £10.00",
+    ]);
   });
 });
